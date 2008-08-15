d7b4f3fbc5b25d62e2212bd311fca553eb760bc4
Anders Kaseorg <andersk@mit.edu>
2008-08-15 17:25:11 -0400

    Move internal ksplice definitions from ksplice.h to ksplice.c.
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- kmodsrc/ksplice.h
+++ kmodsrc/ksplice.h
@@ -27,19 +27,8 @@ struct ksplice_patch {
 };
 
 #ifdef __KERNEL__
-#ifdef CONFIG_DEBUG_FS
-#include <linux/debugfs.h>
-#endif /* CONFIG_DEBUG_FS */
 #include <linux/module.h>
-#include <linux/pagemap.h>
-#include <linux/sched.h>
 #include <linux/version.h>
-
-#if BITS_PER_LONG == 32
-#define ADDR "08lx"
-#elif BITS_PER_LONG == 64
-#define ADDR "016lx"
-#endif /* BITS_PER_LONG */
 
 #if defined(CONFIG_PARAVIRT) && defined(CONFIG_X86_64) &&	\
     LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,25) &&		\
@@ -65,15 +54,6 @@ struct ksplice_patch {
 #define cleanup_ksplice_module KSPLICE_KID_UNIQ(cleanup_ksplice_module)
 #endif
 
-enum ksplice_stage_enum {
-	PREPARING, APPLIED, REVERSED
-};
-
-enum ksplice_abort_cause_enum {
-	NONE, NO_MATCH, BAD_SYSTEM_MAP, CODE_BUSY, MODULE_BUSY, UNEXPECTED,
-	FAILED_TO_FIND, ALREADY_REVERSED
-};
-
 struct module_pack {
 	const char *name;
 	const char *kid;
@@ -97,163 +77,6 @@ struct module_pack {
 	struct list_head safety_records;
 	struct list_head list;
 };
-
-struct update_bundle {
-	const char *kid;
-	const char *name;
-	struct kobject kobj;
-	enum ksplice_stage_enum stage;
-	enum ksplice_abort_cause_enum abort_cause;
-	int debug;
-#ifdef CONFIG_DEBUG_FS
-	struct debugfs_blob_wrapper debug_blob;
-	struct dentry *debugfs_dentry;
-#endif /* CONFIG_DEBUG_FS */
-	struct list_head packs;
-	struct list_head conflicts;
-	struct list_head list;
-};
-
-struct conflict {
-	const char *process_name;
-	pid_t pid;
-	struct list_head stack;
-	struct list_head list;
-};
-
-struct ksplice_frame {
-	unsigned long addr;
-	int has_conflict;
-	const char *symbol_name;
-	struct list_head list;
-};
-
-#if defined(CONFIG_DEBUG_FS) && LINUX_VERSION_CODE < KERNEL_VERSION(2,6,17)
-/* Old kernels don't have debugfs_create_blob */
-struct debugfs_blob_wrapper {
-	void *data;
-	unsigned long size;
-};
-#endif /* CONFIG_DEBUG_FS && LINUX_VERSION_CODE */
-
-struct reloc_nameval {
-	struct list_head list;
-	char *name;
-	unsigned long val;
-	enum { NOVAL, TEMP, VAL } status;
-};
-
-struct reloc_addrmap {
-	struct list_head list;
-	unsigned long addr;
-	struct reloc_nameval *nameval;
-	int pcrel;
-	long addend;
-	int size;
-	long dst_mask;
-};
-
-#if LINUX_VERSION_CODE >= KERNEL_VERSION(2,6,25)
-static inline int virtual_address_mapped(unsigned long addr)
-{
-	unsigned int level;
-	pte_t *pte = lookup_address(addr, &level);
-	return pte == NULL ? 0 : pte_present(*pte);
-}
-#else /* LINUX_VERSION_CODE < */
-/* f0646e43acb18f0e00b00085dc88bc3f403e7930 was after 2.6.24 */
-static inline int virtual_address_mapped(unsigned long addr)
-{
-	pgd_t *pgd = pgd_offset_k(addr);
-#ifdef pud_page
-	pud_t *pud;
-#endif /* pud_page */
-	pmd_t *pmd;
-	pte_t *pte;
-
-	if (!pgd_present(*pgd))
-		return 0;
-
-#ifdef pud_page
-	pud = pud_offset(pgd, addr);
-	if (!pud_present(*pud))
-		return 0;
-
-	pmd = pmd_offset(pud, addr);
-#else /* pud_page */
-	pmd = pmd_offset(pgd, addr);
-#endif /* pud_page */
-
-	if (!pmd_present(*pmd))
-		return 0;
-
-	if (pmd_large(*pmd))
-		return 1;
-
-	pte = pte_offset_kernel(pmd, addr);
-	if (!pte_present(*pte))
-		return 0;
-
-	return 1;
-}
-#endif /* LINUX_VERSION_CODE */
-
-struct reloc_nameval *find_nameval(struct module_pack *pack, char *name,
-				   int create);
-struct reloc_addrmap *find_addrmap(struct module_pack *pack,
-				   unsigned long addr);
-int handle_myst_reloc(struct module_pack *pack, unsigned long pre_addr,
-		      unsigned long run_addr, int rerun);
-
-struct safety_record {
-	struct list_head list;
-	const char *name;
-	unsigned long addr;
-	unsigned int size;
-	int care;
-};
-
-struct candidate_val {
-	struct list_head list;
-	unsigned long val;
-};
-
-#define singular(list) (!list_empty(list) && (list)->next->next == (list))
-
-#ifdef CONFIG_DEBUG_FS
-extern int init_debug_buf(struct update_bundle *bundle);
-extern void clear_debug_buf(struct update_bundle *bundle);
-extern int __ksdebug(struct update_bundle *bundle, const char *fmt, ...);
-#define _ksdebug(bundle, level, fmt, ...)			\
-	do {							\
-		if ((bundle)->debug >= (level))			\
-			__ksdebug(bundle, fmt, ## __VA_ARGS__);	\
-	} while (0)
-#else /* !CONFIG_DEBUG_FS */
-#define _ksdebug(bundle, level, fmt, ...)		\
-	do {						\
-		if ((bundle)->debug >= (level))		\
-			printk(fmt, ## __VA_ARGS__);	\
-	} while (0)
-static inline int init_debug_buf(struct update_bundle *bundle)
-{
-	return 0;
-}
-static inline void clear_debug_buf(struct update_bundle *bundle)
-{
-	return;
-}
-#endif /* CONFIG_DEBUG_FS */
-#define ksdebug(pack, level, fmt, ...) \
-	do { _ksdebug((pack)->bundle, level, fmt, ## __VA_ARGS__); } while (0)
-#define failed_to_find(pack, sym_name) \
-	ksdebug(pack, 0, KERN_ERR "ksplice: Failed to find symbol %s at " \
-		"%s:%d\n", sym_name, __FILE__, __LINE__)
-
-static inline void print_abort(struct module_pack *pack, const char *str)
-{
-	ksdebug(pack, 0, KERN_ERR "ksplice: Aborted. (%s)\n", str);
-}
 
 int init_ksplice_module(struct module_pack *pack);
 void cleanup_ksplice_module(struct module_pack *pack);
