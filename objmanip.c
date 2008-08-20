718f671508d7fcc42acdff82a4d4715ca2fe6cd6
Tim Abbott <tabbott@mit.edu>
2008-08-20 01:10:59 -0400

    Remove assert preventing keep-primary mode from deleting all sections.
    Signed-off-by: Tim Abbott <tabbott@mit.edu>

--- objmanip.c
+++ objmanip.c
@@ -713,8 +713,6 @@ bfd_boolean copy_object(bfd *ibfd, bfd *obfd)
 	   any output is done.  Thus, we traverse all sections multiple times.  */
 	bfd_map_over_sections(ibfd, setup_section, obfd);
 
-	assert(bfd_count_sections(obfd));
-
 	struct supersect *ss;
 	for (ss = new_supersects; ss != NULL; ss = ss->next)
 		setup_new_section(obfd, ss);
