827052f6ef40719269fc1ec8c50b17bfd23fb153
Tim Abbott <tabbott@mit.edu>
2008-10-05 22:20:36 -0400

    Add a generic binary search implementation.
    Signed-off-by: Tim Abbott <tabbott@mit.edu>

--- kmodsrc/ksplice.c
+++ kmodsrc/ksplice.c
@@ -496,6 +496,8 @@ static unsigned long follow_trampolines(struct ksplice_pack *pack,
 static bool patches_module(const struct module *a, const struct module *b);
 static bool starts_with(const char *str, const char *prefix);
 static bool singular(struct list_head *list);
+static void *bsearch(const void *key, const void *base, size_t n,
+		     size_t size, int (*cmp)(const void *key, const void *elt));
 
 /* Debugging */
 static abort_t init_debug_buf(struct update *update);
@@ -2318,6 +2320,25 @@ static bool starts_with(const char *str, const char *prefix)
 static bool singular(struct list_head *list)
 {
 	return !list_empty(list) && list->next->next == list;
+}
+
+static void *bsearch(const void *key, const void *base, size_t n,
+		     size_t size, int (*cmp)(const void *key, const void *elt))
+{
+	int start = 0, end = n - 1, mid, result;
+	if (n == 0)
+		return NULL;
+	while (start <= end) {
+		mid = (start + end) / 2;
+		result = cmp(key, base + mid * size);
+		if (result < 0)
+			end = mid - 1;
+		else if (result > 0)
+			start = mid + 1;
+		else
+			return (void *)base + mid * size;
+	}
+	return NULL;
 }
 
 #ifdef CONFIG_DEBUG_FS
