b3d250a0b4f5496a28b06fca114674902b32aed9
Anders Kaseorg <andersk@mit.edu>
2009-01-27 14:50:18 -0500

    Spell my name correctly.
    Signed-off-by: Anders Kaseorg <andersk@mit.edu>

--- kmodsrc/ksplice.c
+++ kmodsrc/ksplice.c
@@ -1,5 +1,5 @@
 /*  Copyright (C) 2007-2009  Ksplice, Inc.
- *  Authors: Jeff Arnold, Anders Kasoerg, Tim Abbott
+ *  Authors: Jeff Arnold, Anders Kaseorg, Tim Abbott
  *
  *  This program is free software; you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License, version 2.
